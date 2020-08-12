24cedcc28f29ebeee4b1d102f02096bd03858a73
Lucas Pires <luc_50@outlook.com>
2020-08-12 20:27:19 -0300

    Adicionando o React Router Dom

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 import Button from '../Button';
 
 
 function Menu(){
+    //href = to
     return (
         <nav className='Menu'>
-            <a href="/">
+            <Link to="/">
             <img className='Logo' src={Logo} alt='Devflix Logo'/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav> 
